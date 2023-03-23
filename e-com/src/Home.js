ee9e4f44e8adb33f020763c02abdb699eb3dce81
nikita <nikitaagarwal8755@gmail.com>
2023-03-23 21:01:16 +0530

    HeroSection file is added

--- e-com/src/Home.js
+++ e-com/src/Home.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import HeroSection from './Components/HeroSection';
 const Home = () => {
   return (
-    <Wrapper className='test'>Home</Wrapper>
+    <HeroSection />
   )
 }
 
-const Wrapper = styled.section`
-background-color : ${({theme})=>theme.colors.bg};
-width: 20rem;
-height:20rem;
-`;
 export default Home
